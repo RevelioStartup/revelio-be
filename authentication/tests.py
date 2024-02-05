3e5bcb1b7f26816580fa3991402332f44c0332e9
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-02-05 17:11:03 +0700

    Fix Test and SonarCloud

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from authentication.models import *
 
 # Create your tests here.
+class MockTest(TestCase):
+    def testTodo(self):
+        label = "mock"
+        description = "mock"
+        obj = Todo(label=label,description=description)
+        self.assertEqual(obj.label,label)
