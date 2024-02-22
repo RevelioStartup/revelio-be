09721ae087835621a85653dd653d93cf17867c63
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-02-22 17:53:11 +0700

    [RED] Start working on Register functionality. Add base code for the business logic in views.py without implementation and create one unit test.

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,4 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from authentication.models import *
+from django.urls import reverse
+import json
 
-# Create your tests here.
+class RegisterTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def testRegisterValid(self):
+        data = {
+                "username":"user1",
+                "password":"pass1",
+                "email":"email1@email.com"
+        }
+        response = self.client.post(reverse('authentication:register'), json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(AppUser.objects.filter(username='user1').exists())
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,15 @@
+from django.shortcuts import render
+import json
+from django.http import JsonResponse
 from authentication.models import *
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import re
+from django.core.serializers import serialize
 
-# Create your views here.
-
+@method_decorator(csrf_exempt, name='dispatch')
+class RegisterView(View):
+    def post(self, request):
+        return 'None'
