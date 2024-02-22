215b389418cf8685b40d0d96a6ed2853b0e8ab22
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-02-22 18:53:23 +0700

    [RED] Adding new test for email format not valid.

--- authentication/tests.py
+++ authentication/tests.py
@@ -16,3 +16,15 @@ class RegisterTest(TestCase):
         response = self.client.post(reverse('authentication:register'), json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(AppUser.objects.filter(username='user1').exists())
+    
+    def testEmailFormatNotValid(self):
+        data ={
+            "username":"user1",
+                "password":"pass1",
+                "email":"emailnotvalid"
+        }
+        response = self.client.post(reverse('authentication:register'), json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        resp_msg = json.loads(response.content.decode('utf-8'))['msg']
+        self.assertEqual(resp_msg,"Email format is wrong.")
+        self.assertFalse(AppUser.objects.filter(username='user1').exists())
