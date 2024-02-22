73accb2d9ec13729b963aff770fda2f436923443
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-02-22 18:56:59 +0700

    [GREEN] Finish handling worng email format

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ from authentication.models import *
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+import re
 
 @method_decorator(csrf_exempt, name='dispatch')
 class RegisterView(View):
@@ -12,6 +13,8 @@ class RegisterView(View):
         username = data['username']
         password = data['password']
         email = data['email']
+        if re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email) is None:
+            return JsonResponse({'msg': 'Email format is wrong.'}, status=400)
         new_user = AppUser.objects.create_user(email=email,username=username,password=password)
         new_user.save()
         return JsonResponse({'msg': 'Success! User registered'}, status=200)
