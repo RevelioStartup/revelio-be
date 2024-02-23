95d67b65f424a36edd22efe64422a5cfb4495a33
Catherine Angel <catheriner40540@gmail.com>
2024-02-23 22:26:11 +0700

    fix: change python version

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Installing all requirements"
-python3.10 -m pip install -r requirements.txt
+python3.11 -m pip install -r requirements.txt
 
 echo "Migrating"
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
 
 echo "Collect Static"
-python3.10 manage.py collectstatic --noinput --clear
+python3.11 manage.py collectstatic --noinput --clear
