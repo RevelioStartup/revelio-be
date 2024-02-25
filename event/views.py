668bca8b2556796b16c5f2bec75cab958c1b1bef
emiriko <notalvaroaustin@gmail.com>
2024-02-25 16:01:00 +0700

    [GREEN] Implementing Get Event Feature

--- event/views.py
+++ event/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class EventView(APIView):
     def get(self, request):
-        raise NotImplementedError
+        return Response('Event')
