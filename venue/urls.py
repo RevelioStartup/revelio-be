c3c44831c006ec78ac9742f7c415a05f95157424
Catherine Angel <catheriner40540@gmail.com>
2024-02-25 22:35:25 +0700

    [REFACTOR] change url to plural forms

--- venue/urls.py
+++ venue/urls.py
@@ -9,10 +9,10 @@ from .views import (
 )
 
 urlpatterns = [
-    path('venues/', VenueListCreateView.as_view(), name='venue-list-create'),
-    path('venues/<int:pk>/', VenueRetrieveUpdateDestroyView.as_view(), name='venue-retrieve-update-destroy'),
-    path('venues/<int:pk>/status/', VenueStatusUpdateAPIView.as_view(), name='venue-status-update'),
-    path('venues/event/<int:event_id>/', VenueEventListView.as_view(), name='venue-event-list'),
+    path('', VenueListCreateView.as_view(), name='venue-list-create'),
+    path('<int:pk>/', VenueRetrieveUpdateDestroyView.as_view(), name='venue-retrieve-update-destroy'),
+    path('<int:pk>/status/', VenueStatusUpdateAPIView.as_view(), name='venue-status-update'),
+    path('event/<int:event_id>/', VenueEventListView.as_view(), name='venue-event-list'),
     path('photos/', PhotoCreateView.as_view(), name='photo-create'),
     path('photos/<int:pk>/', PhotoRetrieveUpdateDestroyView.as_view(), name='photo-retrieve-update-destroy'),
 ]
