5b78c16a10537aba0654fd4b9a7ccddaf0a6e4df
Natania Deandra <nataniadeandra@gmail.com>
2024-02-25 19:57:22 +0700

    [GREEN] feat: Venue feature

--- venue/models.py
+++ venue/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    price = models.IntegerField()
+    status = models.CharField(max_length=255)
+    contact_name = models.CharField(max_length=255)
+    contact_phone_number = models.CharField(max_length=15)
+    event = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    venue = models.ForeignKey(Venue, related_name='photos', on_delete=models.CASCADE)
+    image = models.URLField()
+
+    def __str__(self):
+        return self.image
--- venue/views.py
+++ venue/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Venue, Photo
+from .serializers import VenueSerializer, PhotoSerializer
+
+
+class VenueListCreateView(generics.ListCreateAPIView):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+class VenueRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+class VenueEventListView(APIView):
+    def get(self, request, *args, **kwargs):
+        event_id = kwargs.get('event_id')
+        venues = Venue.objects.filter(event=event_id)
+        serializer = VenueSerializer(venues, many=True)
+        return Response(serializer.data)
+
+class PhotoCreateView(generics.CreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
+class PhotoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
