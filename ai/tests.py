720c49ff70be54c046c4a1fadcdb288ce3e37930
swastikanata <swastika.nata1804@gmail.com>
2024-02-25 19:55:56 +0700

    Revert "[RED] add test case for valid and invalid prompt for autofilling"
    This reverts commit 7cb1af1eff21babbc9bb2bc4f40bb15f6dea5ceb.

--- ai/tests.py
+++ ai/tests.py
@@ -35,9 +35,7 @@ class AutofillTest(TestCase):
 
     def test_autofill_valid(self):
         form = {
-            'name': 'Ulang tahun Ibu',
-            'date': '26/02/2024',
-            'budget': '300000',
+
         }
         data = {
             'prompt' : create_autofill_prompt(form),
@@ -48,9 +46,7 @@ class AutofillTest(TestCase):
 
     def test_autofill_invalid(self):
         form = {
-            'name': '',
-            'date': '26/02/2024',
-            'budget': '',
+
         }
         data = {
             'prompt' : create_autofill_prompt(form),
