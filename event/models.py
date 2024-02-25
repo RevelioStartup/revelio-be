cc94f2a1307c3c2847feeb704e492d2ad14c533d
emiriko <notalvaroaustin@gmail.com>
2024-02-25 17:14:35 +0700

    feat: add event model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    budget = models.DecimalField(max_digits=20, decimal_places=2)
+    objective = models.TextField()
+    attendees = models.IntegerField()    
+    thmee = models.TextField()
+    services = models.TextField()
+    
+    def __str__(self):
+        return self.name
--- revelio/settings_dev.py
+++ revelio/settings_dev.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'authentication',
     'rest_framework',
     'rest_framework_simplejwt',
+    'event'
 ]
 
 MIDDLEWARE = [
