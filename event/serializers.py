483f9ba7e33270917194bf9facb32a3462c05138
emiriko <notalvaroaustin@gmail.com>
2024-02-25 17:16:01 +0700

    feat: add event serializer

---
+++ event/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from event.models import Event
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
