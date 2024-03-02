42d3625d071f78d72d3b077c6fc08bfc3ff3bd76
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-03-02 08:21:51 +0700

    [CHORES] Add urls for preview FE app.

--- revelio/settings.py
+++ revelio/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'revelio.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
     "https://revelio-two.vercel.app",
+    "https://revelio-reveliostartup-gmailcom-revelios-projects.vercel.app",
 ]
 
 
