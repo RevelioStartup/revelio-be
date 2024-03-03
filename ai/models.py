51f15523d5d95b7e435a1e6d3a5ec306843bf40d
swastikanata <swastika.nata1804@gmail.com>
2024-03-03 11:57:23 +0700

    set up models and serializers for AIRecommendation

--- ai/models.py
+++ ai/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
+
+from authentication.models import AppUser 
 
 # Create your models here.
+class AIRecommendation(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False) 
+    user = models.ForeignKey(AppUser, on_delete = models.CASCADE)
+    date = models.DateField()
+    prompt = models.TextField()
+    output = models.TextField()
