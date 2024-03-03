56b3e1aea53475441f9b5cdceea5ce5d1f492d75
swastikanata <swastika.nata1804@gmail.com>
2024-03-03 15:06:11 +0700

    alter timezone datatype on model to datetime

--- ai/models.py
+++ ai/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from datetime import date
 import uuid
 
 from authentication.models import AppUser 
@@ -8,6 +9,6 @@ from authentication.models import AppUser
 class RecommendationHistory(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False) 
     user = models.ForeignKey(AppUser, on_delete = models.CASCADE)
-    date = models.DateField(default = timezone.now)
+    date = models.DateField(default = date.today)
     prompt = models.TextField()
     output = models.TextField()
