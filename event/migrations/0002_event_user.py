36e39cd4063f140b5459f9e31121a43b6af21d5f
Natania Deandra <nataniadeandra@gmail.com>
2024-03-09 14:36:18 +0700

    fixed migrations on event

--- event/migrations/0002_event_user.py
+++ event/migrations/0002_event_user.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('event', '0002_alter_event_id'),
+        ('event', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
