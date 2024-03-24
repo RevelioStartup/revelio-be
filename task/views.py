c1dbbe7b785981d9fe4d9e667f7f438a8e07b9b1
JohannesSetiawan <88031010+JohannesSetiawan@users.noreply.github.com>
2024-03-24 11:39:24 +0700

    Feat: Implementing Melihat Task List and Shorten Verification Token (#27)
    Changes:
    
    Implementing test and functionality for Melihat Task List.
    Implementing test and functionality to shorten the long verification token (Just 8 characters/digits now) for Account Recovery and Email Verification feature.

--- task/views.py
+++ task/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import RetrieveDestroyAPIView
 
 class TaskCreateView(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class SeeTaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'event_id'
+    permission_classes = [IsAuthenticated]
