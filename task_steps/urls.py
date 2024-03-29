df589fa077261b3ccb19a29dd452ff66f6424e82
swastikanata <swastika.nata1804@gmail.com>
2024-03-29 17:21:42 +0700

    [RED] add url set up for mengubah step by step

--- task_steps/urls.py
+++ task_steps/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     TaskStepCreateView,
+    TaskStepUpdateView,
     TaskStepDestroyView,
     DeleteAllTaskStepsView,
 )
@@ -8,5 +9,6 @@ from .views import (
 urlpatterns = [
     path('', TaskStepCreateView.as_view(), name='task-step-list-create'),
     path('<uuid:pk>/', TaskStepDestroyView.as_view(), name='task-step-detail'),
+    path('<uuid:pk>/edit/', TaskStepUpdateView.as_view(), name='task-step-update'),
     path('tasks/<int:task_id>/delete-all-steps/', DeleteAllTaskStepsView.as_view(), name='delete-all-task-steps'),
 ]
