926beacc3c557a193b62442f1a2a5626cedca7db
swastikanata <swastika.nata1804@gmail.com>
2024-03-29 20:35:49 +0700

    mengubah url rekomendasi ai

--- ai/urls.py
+++ ai/urls.py
@@ -6,6 +6,6 @@ app_name = 'ai'
 urlpatterns = [
     path('autofill/', AutoFillView.as_view(), name='autofill'),
     path('assistant/', AssistantView.as_view(), name='assistant'),
-    path('history/', HistoryView.as_view(), name='history'),
-    path('history/<uuid:id>/', HistoryDetailView.as_view(), name='history-detail'),
+    path('history/all/<str:event_id>/', HistoryView.as_view(), name='history'),
+    path('history/<str:id>/', HistoryDetailView.as_view(), name='history-detail'),
 ]
