4d69e16785ee295f3b45b766a46f9b11f02330c9
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-04-03 08:48:12 +0700

    [CHORES] Sort Steps by its step_order.

--- task/serializers.py
+++ task/serializers.py
@@ -5,8 +5,15 @@ from task_steps.serializers import TaskStepSerializer
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
-    task_steps = TaskStepSerializer(many=True, read_only=True)
+    task_steps = serializers.SerializerMethodField()
     
     class Meta:
         model = Task
         fields = '__all__'
+
+    def get_task_steps(self, obj):
+        task_steps = obj.task_steps.all().order_by('step_order')
+
+        serializer = TaskStepSerializer(instance=task_steps, many=True, read_only=True)
+
+        return serializer.data
