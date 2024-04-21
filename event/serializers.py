87d4af735c9617e51f63cc1f2afeef3c038cfce2
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-04-21 19:12:04 +0700

    [REFACTOR] Add rundowns to EventSerializers.

--- event/serializers.py
+++ event/serializers.py
@@ -3,9 +3,12 @@ from rest_framework import serializers
 from event.models import Event
 from vendor.serializers import VendorSerializer
 from venue.serializers import VenueSerializer
+from rundown.serializers import RundownSerializer
+from rundown.models import Rundown
 
 
 class EventSerializer(serializers.ModelSerializer):
+    rundowns = serializers.SerializerMethodField()
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     venues = VenueSerializer(many=True, read_only=True)
     vendors = VendorSerializer(many=True, read_only=True)
@@ -13,3 +16,10 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+    
+    def get_rundowns(self, obj):
+        rundowns = Rundown.objects.filter(event_id = obj.id).order_by('rundown_order')
+
+        serializer = RundownSerializer(instance=rundowns, many=True, read_only=True)
+
+        return serializer.data
