fad2d70d531fce9d23dff32990f58fdb2899e4a7
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-04-21 19:21:53 +0700

    [CHORES] Add swagger.

--- rundown/views.py
+++ rundown/views.py
@@ -5,9 +5,33 @@ from .models import Rundown
 from .serializers import RundownSerializer
 from rest_framework.response import Response
 from .validators import validate_rundown_data, is_valid_updated_data
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class RundownCreateView(APIView):
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['event_id', 'rundown_data'],
+            properties={
+                'event_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Event ID'),
+                'rundown_data': openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    description='List of rundowns',
+                    items=openapi.Items(
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            'description': openapi.Schema(type=openapi.TYPE_STRING, description='Activity that time in rundown'),
+                            'start_time': openapi.Schema(type=openapi.TYPE_STRING, description='Start time of the activity'),
+                            'end_time': openapi.Schema(type=openapi.TYPE_STRING, description='End time of the activity'),
+                        }
+                    )
+                ),
+            },
+        ),
+        responses={201: RundownSerializer(many=True), 400: 'Bad Request', 500: 'Internal Server Error'}
+    )
     def post(self, request):
         event_id = request.data.get('event_id')
         rundown_data = request.data.get('rundown_data')
@@ -32,6 +56,18 @@ class RundownCreateView(APIView):
         return Response(rundown_serializers.data, status=201)    
     
 class RundownUpdateView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['description', 'start_time, end_time'],
+            properties={
+                'description': openapi.Schema(type=openapi.TYPE_STRING, description='Activity that time in rundown'),
+                'start_time': openapi.Schema(type=openapi.TYPE_STRING, description='Start time of the activity'),
+                'end_time': openapi.Schema(type=openapi.TYPE_STRING, description='End time of the activity'),
+            },
+        ),
+        responses={200: RundownSerializer(), 400: 'Bad Request', 500: 'Internal Server Error'}
+    )
     def patch(self, request, id):
         new_start_time = request.data.get("start_time")
         new_end_time = request.data.get("end_time")
