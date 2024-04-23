d1e4f47df6cddb295dcabd34e66b565f2dc1618f
carlenee <carleneannabel@gmail.com>
2024-04-23 19:03:15 +0700

    [REFACTOR] remove unused import

--- timeline/models.py
+++ timeline/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 import uuid
-from event.models import Event
 from task_steps.models import TaskStep
 from datetime import datetime
-from django.core.exceptions import ValidationError
 
 class Timeline(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
