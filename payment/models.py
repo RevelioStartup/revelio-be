4beb71e97529cf8c99266f628e61d69d7da07c73
Catherine Angel <catheriner40540@gmail.com>
2024-04-30 12:50:58 +0700

    [RED] add test for payment

--- payment/models.py
+++ payment/models.py
@@ -6,32 +6,23 @@ from authentication.models import AppUser
 class Transaction(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False) 
     user = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name='transactions')
-    midtrans_id = models.CharField(max_length=1024, unique=True, db_index=True)
+
+    midtrans_token = models.CharField(max_length=1024, unique=True, db_index=True, null=True)
+    midtrans_transaction_id = models.CharField(max_length=1024, unique=True, db_index=True)
     order_id = models.CharField(max_length=1024, unique=True, db_index=True)
 
     price = models.BigIntegerField()
-    checkout_date = models.DateTimeField(auto_now_add=True)
-    expiry_date = models.DateTimeField()
+    checkout_time = models.DateTimeField(auto_now_add=True)
+    expiry_time = models.DateTimeField(null=True, blank=True)
 
-    payment_method = models.CharField(max_length=30, blank=True, null=True)
-    payment_channel = models.CharField(max_length=30, blank=True, null=True)
     payment_type = models.CharField(max_length=30, blank=True, null=True)
-    payment_destination = models.CharField(max_length=1024, blank=True, null=True)
+    payment_merchant = models.CharField(max_length=1024, blank=True, null=True)
 
-    transaction_details = models.TextField(blank=True, null=True)  # JSON response
-    paid_at_date = models.DateTimeField(blank=True, null=True)
-
-    status = models.CharField(max_length=16, default='PENDING', choices=[
-        ('PENDING', 'Pending'),
-        ('CANCELLED', 'Cancelled'),
-        ('PROCESSING', 'Processing'),
-        ('SUCCESS', 'Success'),
-        ('REFUND', 'Refund'),
-        ('EXPIRED', 'Expired'),
-    ], db_index=True)
+    paid_at_time = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=30, blank=True, null=True)
 
     class Meta:
-        ordering = ['-checkout_date']
+        ordering = ['-checkout_time']
 
     def __str__(self):
         return f"{self.order_id} - {self.status}"
--- payment/tests.py
+++ payment/tests.py
@@ -1,3 +1,64 @@
-from django.test import TestCase
+import os
+from unittest.mock import patch
+from rest_framework import status
+from django.urls import reverse
+from .models import Transaction
+from authentication.models import AppUser
+from rest_framework.test import APIClient, APITestCase
 
-# Create your tests here.
+SNAP_API = 'payment.views.SNAP_API'
+
+class CreatePaymentViewTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = AppUser.objects.create_user(email='user@example.com', username='testuser', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+
+        self.url = reverse('payment:create-payment') 
+
+    @patch(SNAP_API)
+    def test_create_payment_success(self, mock_snap_api):
+        mock_snap_api.create_transaction.return_value = {
+            'token': 'dummy_token',
+            'redirect_url': 'http://dummy_redirect_url.com'
+        }
+
+        data = {'order_id': 'test_order', 'amount': 100}
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('redirect_url', response.data)
+
+        self.assertEqual(Transaction.objects.count(), 1)
+        self.assertTrue(Transaction.objects.filter(order_id='test_order').exists())
+
+    @patch(SNAP_API)
+    def test_get_payment_details(self, mock_snap_api):
+        mock_snap_api.transactions.status.return_value = {
+            'payment_type': 'dummy_payment_type',
+            'acquirer': 'dummy_acquirer',
+            'transaction_time': '2024-04-30 00:27:59',
+            'expiry_time': '2024-04-30 00:42:59',
+            'transaction_status': 'dummy_transaction_status',
+            'transaction_id': 'dummy_transaction_id',
+            'status_message': 'dummy_status_message'
+        }
+
+        transaction = Transaction.objects.create(order_id='test_order',price=100 ,user=self.user)
+
+        response = self.client.get(f"{self.url}?order_id=test_order")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('transaction_detail', response.data)
+    
+    def test_missing_order_id_parameter(self):
+        response = self.client.get(self.url) 
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @patch(SNAP_API)
+    def test_get_payment_details_failure(self, mock_snap_api):
+        mock_snap_api.transactions.status.return_value = None
+        response = self.client.get(f"{self.url}?order_id=test_order")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
