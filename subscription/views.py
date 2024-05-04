304a7d0f33d264b76c4f09388e05c5973080539e
emiriko <notalvaroaustin@gmail.com>
2024-05-04 20:36:11 +0700

    [RED]: Adding latest subscription endpoint and permission for premium user

--- subscription/views.py
+++ subscription/views.py
@@ -4,14 +4,23 @@ from rest_framework.response import Response
 
 from subscription.models import Subscription
 from subscription.serializers import SubscriptionSerializer
+from utils.permissions import isPremiumUser
 
 # Create your views here.
 class SubscriptionHistory(generics.RetrieveAPIView):
+    permission_classes = [isPremiumUser]
+    
     def get(self, request):
-        subscription = Subscription.objects.filter(user=request.user)
-        
-        new_subscription = [ sub for sub in subscription if not sub.is_active ]
-        
+        subscription = Subscription.objects.filter(user=request.user).order_by('-start_date') # Ordering
+                
         serializer = SubscriptionSerializer(subscription, many=True)
+        
+        return Response(serializer.data)
+    
+class LatestSubscription(generics.RetrieveAPIView):
+    def get(self, request):
+        subscription = Subscription.objects.filter(user=request.user).latest('start_date')
+        
+        serializer = SubscriptionSerializer(subscription)
         
         return Response(serializer.data)
