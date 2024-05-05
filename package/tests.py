d9712c228786f2b105278271426180a6911e66dd
Natania Deandra <nataniadeandra@gmail.com>
2024-05-05 13:57:48 +0700

    [REFACTOR] Delete duplicate import and unnecessary comment

--- package/tests.py
+++ package/tests.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
