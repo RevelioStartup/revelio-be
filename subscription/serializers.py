464d4a599d800f15b6e572f7a3cb5824ddce372c
emiriko <notalvaroaustin@gmail.com>
2024-05-06 12:02:22 +0700

    [RED] Implementing nested serialize

--- subscription/serializers.py
+++ subscription/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from package.serializers import PackageSerializer
 from subscription.models import Subscription
 
 
@@ -7,4 +8,10 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = ['user', 'plan', 'start_date', 'end_date', 'is_active']
+        
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['plan'] = PackageSerializer(instance.plan).data
+        
+        return response
         
