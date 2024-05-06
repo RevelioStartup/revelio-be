a9b4b6b41443b7ee64a7e5eb33d0f6be7b9a0ff2
emiriko <notalvaroaustin@gmail.com>
2024-05-06 12:24:49 +0700

    feat: finishing adding permission

--- utils/permissions.py
+++ utils/permissions.py
@@ -31,3 +31,74 @@ class IsPremiumUser(BasePermission):
         is_active_subscriptions = [True for subscription in subscriptions if subscription.is_active]
         
         return len(is_active_subscriptions) > 0
+    
+def get_current_subscription(user):
+    subscriptions = Subscription.objects.filter(user = user).order_by('-start_date')
+    is_active_subscriptions = [subscription for subscription in subscriptions if subscription.is_active]
+    
+    return is_active_subscriptions[0] if len(is_active_subscriptions) > 0 else None
+class HasEventPlanner(BasePermission):
+    """
+    Permission to check whether the current user can access event planner
+    """
+    
+    def has_permission(self, request, view):
+        user = request.user
+        current_subscription = get_current_subscription(user)
+        
+        if current_subscription:
+            return current_subscription.plan.event_planner
+        
+        return False
+    
+class HasEventTracker(BasePermission):
+    """
+    Permission to check whether the current user can access event tracker
+    """
+    
+    def has_permission(self, request, view):
+        user = request.user
+        current_subscription = get_current_subscription(user)
+        
+        if current_subscription:
+            return current_subscription.plan.event_tracker
+        return False
+    
+class HasEventTimeline(BasePermission):
+    """
+    Permission to check whether the current user can access event timeline
+    """
+    
+    def has_permission(self, request, view):
+        user = request.user
+        current_subscription = get_current_subscription(user)
+        
+        if current_subscription:
+            return current_subscription.plan.event_timeline
+        return False
+    
+class HasEventRundown(BasePermission):
+    """
+    Permission to check whether the current user can access event rundown
+    """
+    
+    def has_permission(self, request, view):
+        user = request.user
+        current_subscription = get_current_subscription(user)
+        
+        if current_subscription:
+            return current_subscription.plan.event_rundown
+        return False
+
+class HasAIAssistant(BasePermission):
+    """
+    Permission to check whether the current user can access AI assistant
+    """
+    
+    def has_permission(self, request, view):
+        user = request.user
+        current_subscription = get_current_subscription(user)
+        
+        if current_subscription:
+            return current_subscription.plan.ai_assistant
+        return False
