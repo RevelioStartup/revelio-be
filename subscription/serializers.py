8126011b2ffafa3052b6e8edda6edb2771844280
emiriko <notalvaroaustin@gmail.com>
2024-05-06 19:47:25 +0700

    fix: serializer

--- subscription/serializers.py
+++ subscription/serializers.py
@@ -7,7 +7,7 @@ from subscription.models import Subscription
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
-        fields = ['user', 'plan', 'start_date', 'end_date', 'is_active']
+        fields = ['id', 'user', 'plan', 'start_date', 'end_date', 'is_active']
         
     def to_representation(self, instance):
         response = super().to_representation(instance)
