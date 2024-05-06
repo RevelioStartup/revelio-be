ac860fc56367e9f7c7ec55dfa236b80432918ba3
emiriko <notalvaroaustin@gmail.com>
2024-05-06 11:59:41 +0700

    [GREEN] Fix the test case

--- subscription/tests.py
+++ subscription/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 
 from authentication.models import AppUser
+from package.models import Package
 from subscription.models import Subscription
 from django.urls import reverse
 
@@ -15,7 +16,9 @@ class SubscriptionHistoryTestCase(TestCase):
         self.user = AppUser.objects.create_user(email='user@example.com', username='testuser', password='testpassword')
         self.client.force_authenticate(user=self.user)
         
-        self.subscription = Subscription.objects.create(user=self.user, plan='PREMIUM', end_date=timezone.now() + timedelta(days=30))
+        self.package = Package.objects.create(name='PREMIUM', price=100, duration=30, event_planner=True, event_tracker=True, event_timeline=True, event_rundown=True, ai_assistant=True)
+        
+        self.subscription = Subscription.objects.create(user=self.user, plan=self.package, end_date=timezone.now() + timedelta(days=30))
         self.url = reverse('history') 
         
     def test_get_subscription_history(self):
