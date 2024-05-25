126caa57d9a46aa5068f1d9071c3a2835bad6659
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-05-25 11:33:40 +0700

    [CHORES] Add Vendor and TaskStep to admin page.

--- task_steps/admin.py
+++ task_steps/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TaskStep
 
 # Register your models here.
+admin.site.register(TaskStep)
--- vendor/admin.py
+++ vendor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Vendor, PhotoVendor
 
 # Register your models here.
+admin.site.register(Vendor)
+admin.site.register(PhotoVendor)
