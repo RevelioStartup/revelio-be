feff205c4adbf6fbc561a87d2d4720aa18db3ff4
JohannesSetiawan <88031010+JohannesSetiawan@users.noreply.github.com>
2024-05-28 15:25:53 +0700

    Fix registration flow (2)
    What fixed:
    -) Restrict so that only verified user can logged in.
    -) Fix so system won't sent another email when there's still active token.
    -) Changes token time limit to 25 minutes.
    -) Make sure that every token can only be used once (delete the token every time user use it to verify their email or recover password).

--- authentication/views.py
+++ authentication/views.py
@@ -82,12 +82,14 @@ class LoginView(APIView):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(request, username=username,password=password)
-        if user is not None:
+        if user is not None and AppUser.objects.get(id=user.pk).is_verified_user:
             refresh = RefreshToken.for_user(user)
             return Response({'refresh': str(refresh),
                              'access': str(refresh.access_token)})
-        else:
+        elif user is None:
             return Response({'msg': 'Wrong username/password!'}, status=400)
+        else:
+            return Response({'msg': 'You have not verified yet. Please register again and verify your account!'}, status=400)
         
 class SendVerificationEmailView(APIView):
 
@@ -96,6 +98,10 @@ class SendVerificationEmailView(APIView):
     def get(self, request):
         if request.user.is_verified_user != True:
             user = request.user
+            if UserToken.objects.filter(user=user).exists():
+                token = UserToken.objects.get(user=user).token
+                if account_token.check_token(user, token):
+                    return Response({'msg': 'Token already delivered!'})
             token = create_shortened_token(user)
             asyncio.run(send_verification_email(user, token.upper()))
             return Response({'msg': 'Email delivered!'})
@@ -113,6 +119,7 @@ class SendVerificationEmailView(APIView):
                 if account_token.check_token(user, token):
                     user.is_verified_user = True
                     user.save()
+                    UserToken.objects.filter(user=user).delete()
                     Subscription.objects.create(user=user, plan=Package.objects.get(id=1), start_date=timezone.now(), end_date=timezone.make_aware(datetime(year=9999, month=12, day=31)))
                     return Response({'message': 'Email verified successfully!'}, status=200)
                 else:
@@ -145,6 +152,10 @@ class SendRecoverPasswordEmailView(APIView):
         is_user_exist = AppUser.objects.filter(email=email).exists()
         if is_user_exist:
             user = AppUser.objects.get(email=email)
+            if UserToken.objects.filter(user=user).exists():
+                token = UserToken.objects.get(user=user).token
+                if account_token.check_token(user, token):
+                    return Response({'msg': 'Token already delivered!'})
             token = create_shortened_token(user).upper()
             asyncio.run(send_recover_account_email(user, token))
             return Response({'msg': 'Email delivered!'})
@@ -165,6 +176,7 @@ class SendRecoverPasswordEmailView(APIView):
                 if account_token.check_token(user, token):
                     user.set_password(new_password)
                     user.save()
+                    UserToken.objects.filter(user=user).delete()
                     return Response({'msg': 'Password changes successfully!'}, status=200)
                 else:
                     return Response({'msg': 'Expired token!'}, status=400)
